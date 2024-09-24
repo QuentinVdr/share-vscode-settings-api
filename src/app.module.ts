cd0348c77940db441246f44911472b8dceb6c1eb
Quentin  VERDIER <quent1.verdier@gmail.com>
2024-09-24 15:10:45 +0200

    ✨ integrate ConfigModule and MongooseModule into AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { VscodeConfigModule } from './vscodeConfig/vscodeConfig.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(process.env.DATABASE_URL), VscodeConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
