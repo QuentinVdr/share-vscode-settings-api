bd16ab43b6ddb9a71611948a751e84e9bcc15b27
Quentin VERDIER <quent1.verdier@gmail.com>
2024-10-01 01:17:44 +0200

    🔧 Added ThrottlerModule for rate limiting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,23 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ThrottlerModule } from '@nestjs/throttler';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { VscodeConfigModule } from './vscodeConfig/vscodeConfig.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(process.env.DATABASE_URL), VscodeConfigModule],
+  imports: [
+    ConfigModule.forRoot(),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 10,
+      },
+    ]),
+    MongooseModule.forRoot(process.env.DATABASE_URL),
+    VscodeConfigModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
